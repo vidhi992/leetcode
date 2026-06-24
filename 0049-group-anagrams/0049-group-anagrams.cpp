9b59e49b53aaebaaddfc7650c904309c3e49c9a1
vidhi jain <jainvidhi962@gmail.com>
2026-06-24 21:43:57 +0530

    Time: 19 ms (45.36%), Space: 25.1 MB (62.04%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for(auto s : strs){
+            string t = s;
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
