aa05faff9d1adbc403e117a64374de625b7a86b4
vidhi jain <jainvidhi962@gmail.com>
2026-06-25 22:50:06 +0530

    Time: 17 ms (90.2%), Space: 42.5 MB (22%) - LeetHub

---
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -0,0 +1,57 @@
+class Fenwick {
+public:
+    vector<int> bit;
+    int n;
+
+    Fenwick(int n) {
+        this->n = n;
+        bit.assign(n + 1, 0);
+    }
+
+    void update(int idx, int val) {
+        while (idx <= n) {
+            bit[idx] += val;
+            idx += idx & -idx;
+        }
+    }
+
+    int query(int idx) {
+        int sum = 0;
+        while (idx > 0) {
+            sum += bit[idx];
+            idx -= idx & -idx;
+        }
+        return sum;
+    }
+};
+
+class Solution {
+public:
+    int countMajoritySubarrays(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        vector<int> pref(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            pref[i + 1] = pref[i] + (nums[i] == target ? 1 : -1);
+        }
+
+        vector<int> vals = pref;
+        sort(vals.begin(), vals.end());
+        vals.erase(unique(vals.begin(), vals.end()), vals.end());
+
+        Fenwick ft(vals.size());
+
+        long long ans = 0;
+
+        for (int x : pref) {
+            int idx = lower_bound(vals.begin(), vals.end(), x) - vals.begin() + 1;
+
+            // Count previous prefix sums smaller than current
+            ans += ft.query(idx - 1);
+
+            ft.update(idx, 1);
+        }
+
+        return (int)ans;
+    }
+};
