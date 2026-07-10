dec34319d45d7087145d41dd72247a31dde8f519
vidhi jain <jainvidhi962@gmail.com>
2026-07-10 11:06:19 +0530

    Time: 0 ms (100%), Space: 9.9 MB (25.71%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+        return false;
+
+    unordered_map<char, int> mp;
+
+    for (char c : s)
+        mp[c]++;
+
+    for (char c : t) {
+        if (mp[c] == 0)
+            return false;
+        mp[c]--;
+    }
+
+    return true;
+    }
+};
