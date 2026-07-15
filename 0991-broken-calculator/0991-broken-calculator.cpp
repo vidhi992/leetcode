bb21442e87d53427b2cfa8d2f407a5b84ba4708a
vidhi jain <jainvidhi962@gmail.com>
2026-07-15 09:18:34 +0530

    Time: 0 ms (100%), Space: 7.7 MB (84.49%) - LeetHub

--- 0991-broken-calculator/0991-broken-calculator.cpp
+++ 0991-broken-calculator/0991-broken-calculator.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     int brokenCalc(int startValue, int target) {
         int cnt =0;
-        while(target>startValue){
-    if(target % 2)
-        target++;
-    else
+        while(target > startValue)
+{
+    if(target % 2 == 0)
         target /= 2;
+    else
+        target++;
 
     cnt++;
 }
+
+
 
 return cnt + (startValue - target);
     }
